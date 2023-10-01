2c73facbc26a93b1e41ed44c4e0a5fd20e3fe35e
Saakshi Kobarne <115942356+Saakshik15@users.noreply.github.com>
2023-10-01 13:09:11 +0530

    Create 1. Fibonacci Number.cpp

---
+++ Week-1/2. Recursion/1. Fibonacci Number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int fib(int n){
+
+	//base case
+	if(n==0) return 0;
+	if(n==1) return 1;
+
+	int nth= fib(n-1) + fib(n-2);
+	return nth;
+}
+
+int main(){
+  
+	int n;
+	cin>>n;
+	
+	int ans= fib(n);
+	cout<<ans;
+	
+	return 0;
+}
