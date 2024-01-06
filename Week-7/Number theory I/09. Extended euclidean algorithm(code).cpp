25f185acc43ad87e3c0b4ede15b8ccd95104d8d4
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-01-06 16:00:31 +0530

    Create 09. Extended euclidean algorithm(code).cpp

---
+++ Week-7/Number theory I/09. Extended euclidean algorithm(code).cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int extendedGCD(int a, int b, int& x, int& y){
+
+  if(b==0){
+    x=1; y=0;
+    return a;
+  }
+  
+  int x1, y1;
+
+  int g= extendedGCD(b, a%b, x1, y1);
+  x= y1, y= x1- (a/b)* y1;
+
+  return g;
+}
+
+int main(){
+
+  int a,b,c;
+  int g,x,y;
+  
+  cin>>a>>b>>c;
+
+  g= extendedGCD(a, b, x, y);
+
+  if(c%g == 0) cout<< "x= " <<x<<" & y = " <<y<<"\n";
+  else cout<<"Solution of this LDE does not exist\n";
+  
+}
