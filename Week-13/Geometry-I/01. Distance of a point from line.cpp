0a663cbd36dba71622ef28b697e276b1a1be60e5
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-02-26 19:50:54 +0530

    Create 01. Distance of a point from line.cpp

---
+++ Week-13/Geometry-I/01. Distance of a point from line.cpp
@@ -0,0 +1,10 @@
+/*
+Suppose we are given a line ( ax+ by+ c= 0 ) and a point P(x1, y1)
+Then, the distance of point P from line can be calculated as:
+
+d= |ax1 + by1 + c| / root(a^2 + b^2)
+
+a,b,c and x1, y1 would be given as input in this case and we can use inbuilt functions like abs(), sqrt() to evaluate these.
+
+i.e., d= abs( a*x1 + b*y1 + c ) / sqrt(a^2 + b^2)
+*/
