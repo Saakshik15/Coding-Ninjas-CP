c0568eec39861fb366c6c388dc5ed16c04199cb5
Saakshi Kobarne <115942356+saakshikobarne@users.noreply.github.com>
2024-03-03 19:34:22 +0530

    Create 01. Introduction.cpp

---
+++ Week-14/Game theory-I/01. Introduction.cpp
@@ -0,0 +1,10 @@
+/*
+  
+Combinatorical game:
+1. 2 person game
+2. No hidden info from the players
+3. No chance moves (eg dice throw which is luck based)
+4. Only 3 possible outcomes of the game should be: win loss draw
+5. Players will have alternative moves
+
+*/
